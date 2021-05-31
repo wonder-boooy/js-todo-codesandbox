949c6d28e9a9a50f23a9fbdca0b75c061edc94e9
yuma wada <yuma.wada.1127@gmail.com>
2021-05-31 09:21:55 +0900

    AddTextFunction

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  if (!inputText) {
+    return;
+  }
+
+  document.getElementById("add-text").value = "";
+
+  // リスト生成
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "削除";
+
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(removeButton);
+  li.appendChild(div);
+
+  document.getElementById("imcomplete-list").appendChild(li);
+  // const completeButton = document
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
