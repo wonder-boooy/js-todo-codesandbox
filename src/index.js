db3f565b5d53a27a4f229173417cf7a3e6343b74
yuma wada <yuma.wada.1127@gmail.com>
2021-05-31 18:04:56 +0900

    CompleteFunction

--- src/index.js
+++ src/index.js
@@ -17,15 +17,40 @@ const onClickAdd = () => {
   const p = document.createElement("p");
   p.innerText = inputText;
 
+  // 完了機能
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
-  completeButton.addEventListener("click", () => alert("完了"));
+  completeButton.addEventListener("click", () => {
+    // まず未完了エリアから削除
+    removeFromIncompleteList(completeButton.parentNode.parentNode);
+    // 次に完了エリアに追加
+    const addTarget = completeButton.parentNode.parentNode;
+    const text = addTarget.firstElementChild.firstElementChild.innerText;
+
+    addTarget.textContent = null;
+    const div = document.createElement("div");
+    div.className = "list-row";
+    const p = document.createElement("p");
+    p.innerText = inputText;
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    div.appendChild(p);
+    div.appendChild(backButton);
+    addTarget.appendChild(div);
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  // 削除機能
   const removeButton = document.createElement("button");
   removeButton.innerText = "削除";
   removeButton.addEventListener("click", () => {
-    const removeTarget = removeButton.parentNode.parentNode;
-    document.getElementById("incomplete-list").removeChild(removeTarget);
+    removeFromIncompleteList(removeButton.parentNode.parentNode);
   });
+
+  const removeFromIncompleteList = (target) => {
+    document.getElementById("incomplete-list").removeChild(target);
+  };
 
   div.appendChild(p);
   div.appendChild(completeButton);
